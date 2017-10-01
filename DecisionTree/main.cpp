5a1dfb75a7dfcddddd9ce75e2833e7a210b34b58
nitishplus98 <nitishplus98@gmail.com>
2017-10-01 12:31:59 +0530

    Add * formatting for couts in main.cpp

--- DecisionTree/main.cpp
+++ DecisionTree/main.cpp
@@ -7,14 +7,14 @@ using namespace std;
 int main()
 {
 	Tree t("./raw_data/data.csv","./data/description.txt");
-	cout<<"*********** ID3 ***********"<<endl;
+	cout<<"***********      ID3      ***********"<<endl;
 	t.runtest("./raw_data/testdata.csv");
 	cout<<endl;
-	cout<<"*********** Pruning ***********"<<endl;
+	cout<<"***********    Pruning    ***********"<<endl;
 	PrunedTree p("./raw_data/data.csv","./data/description.txt");
 	p.runtest("./raw_data/testdata.csv");
 	cout<<endl;
-	cout<<"***********Random Forest***********"<<endl;
+	cout<<"*********** Random Forest ***********"<<endl;
 	RandomForest r("./raw_data/data.csv","./data/description.txt");
 	r.runtest("./raw_data/testdata.csv");
 	return 0;
