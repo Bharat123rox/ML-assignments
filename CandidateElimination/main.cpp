15f43ddef657ee6b1b890ec13fd6588b8827338e
Bhk <bharatraghunthan9767@gmail.com>
2017-10-01 15:57:54 +0530

    Added HTML and Word Documentation for Candidate Elimination
    Intro Docx Coming Soon

--- CandidateElimination/main.cpp
+++ CandidateElimination/main.cpp
@@ -4,12 +4,21 @@ using namespace std;
 #define Q 1000000007
 #define NL -Q
 
+/*!
+   \class Reader
+ * This class reads input from the data file and stores it into data
+   \sa printv(),printvov(),read(),data
+ */
 class Reader
 {
 	public:
+	/*! 
+	 * Prints a vector according to hypothesis format.
+	   \param v The vector to be printed.
+	   \sa printvov()
+	 */
 	void printv(vector<int> v)
 	{
-		//Prints vector.
 		for(int i=0;i<v.size();i++){
 			if(v[i]==Q) cout<<"all ";
 			else if(v[i]==NL) cout<<"NULL ";
@@ -17,16 +26,27 @@ class Reader
 		}
 		cout<<endl;
 	}
+	/*! 
+	 * Prints a vector of vectors according to hypothesis format.
+	   \param v The vector of vectors.
+	   \sa printv()
+	*/
 	void printvov(vector< vector<int> > v)
-	{
-		//Prints vector of vector.
+	{   
 		for(int i=0;i<v.size();i++) printv(v[i]);
 	}
-	vector< vector<int> > data;	//Stores data read from the file.
+	//! Stores data read from the file.
+	vector< vector<int> > data;	
 	private:
+	/*!	
+		\fn read() 
+	 * Reads the data and stores it into the variable data.
+	 * Furthermore, the target values are also stored in the variable data.  
+	    \param str File name where data is stored.
+	    \sa Reader()
+	*/
 	void read(string str)
 	{
-		//Reads the data and stores it. str is file name where data is stored.
 		string row,item;
 		fstream inp(str,std::fstream::in);
 		if(!inp){
@@ -44,39 +64,69 @@ class Reader
 				s>>r;
 				res.push_back(r);
 		    }
-			data.push_back(res);
+			data.push_back(res);	
 		    res.clear();
 		}
 		inp.close();
 	}
 	public:
+	/*! Constructor for the class Reader()
+		\param str File name is given here.
+		\sa read() 
+	*/
 	Reader(string str)
 	{
 		read(str);
 	}
 };
 
+/*! 
+	\class CandidateElimination	
+ *  The class which performs the actual Candidate Elimination Algorithm.
+ 	\sa general(), specific(), mingen(), minspec(), compute_boundaries()
+ */
 class CandidateElimination
 {
-	vector< vector<int> > gb;		//General boundary
-	vector<int> sb,q,nl;			//Specific boundary, most general hypothesis, most specific hypothesis, repectively.
-	vector< vector<int> > data;	    //Data for the algo.
-	int target;						//Value of the target concept.
-	vector<vector<int> > vals;		//Value which each attribute can take.
+	vector< vector<int> > gb;		///<General Boundary
+	vector<int> sb;					///<Specific Boundary
+	vector<int> q;					///<Most general hypothesis
+	vector<int> nl;					///<Most specific hypothesis
+	vector< vector<int> > data;	    ///<Data for the algo.
+	int target;						///<Value of the target concept.
+	vector<vector<int> > vals;		///<Value which each attribute can take.
+	/*!
+		\fn general()
+	 *  Compares two hypotheses to see which is more general.	
+	   \param 	v1 First hypothesis to be compared.
+	   \param   v2 Second hypothesis to be compared. 
+	   \return true if v1 is strictly general than v2, False otherwise.
+	 */
 	bool general(vector<int> &v1,vector<int> &v2)
 	{ 
-		//Returns true if v1 is strictly general than v2, false otherwise
 		for(int i=0;i<v1.size();i++) if(((v1[i]!=Q&&v1[i]!=NL&&v1[i]!=v2[i]&&v2[i]!=NL)||(v1[i]==NL&&v2[i]!=NL))) return false;
 		return true;
 	}
+	/*!
+		\fn specific()
+	 * Compares two hypotheses to see which is more specific.
+	    \param 	 v1  First hypothesis to be compared.
+	    \param   v2  Second hypothesis to be compared.
+	 	\return true if v1 is strictly specific than v2, false otherwise
+	 	\sa general()
+	 */	
 	bool specific(vector<int> &v1,vector<int> &v2)
 	{ 
-		//Returns true if v1 is strictly general than v2, false otherwise
 		return general(v2,v1);
 	}
+	/*!
+	    \fn mingen()
+	 * Function to check Minimum Generalization.
+	 	\param v1 Hypothesis for which minimum generalization is required
+	 	\param v2 Concept to be satisfied. 
+		\return Minimum generalization of v1 which satifies v2.
+	 */	
 	vector<int> mingen(vector<int>& v1, vector<int>& v2)
 	{
-		//Returns minimum generalization of v1 which satifies v2.
 		vector<int> ret;
 		for(int i=0;i<v1.size();i++){
 			if(v1[i]==NL) ret.push_back(v2[i]);
@@ -85,9 +135,15 @@ class CandidateElimination
 		}
 		return ret;
 	}
+	/*!
+	    \fn minspec()
+	 *  Function to check Minimum Specialization. 
+	 	\param v1 Hypothesis for which minimum specialization is required.
+	 	\param v2 Concept to be checked against.
+	 	\return Minimum specialization of v1 which does not satify v2.
+	 */	
 	vector< vector<int> > minspec(vector<int>& v1, vector<int>& v2)
 	{
-		//Returns minimum specialization of v1 which does not satify v2.
 		vector< vector<int> > ret;
 		vector<int> tmp=v1;
 		if(v1==v2) return ret;
@@ -106,9 +162,13 @@ class CandidateElimination
 		}
 		return ret;
 	}
+	/*!
+	\fn compute_boundaries()
+	 * Computes General Boundary and Specific Boundary. 
+	 * Follows candidate elimination algorithm, (Refer Machine Learning, Tom Mitchell) 
+	 */
 	void compute_boundaries()
 	{
-		//Computes gb and sb. Follows candidate elimination algorithm, (Refer Machine Learning, Tom Mitchell) 
 		for(auto td:data){
 			int tar=td[td.size()-1];
 			auto hyp=td;
@@ -160,10 +220,17 @@ class CandidateElimination
 				}
 			}
 		}
-		if(gb.empty()||sb.empty()) gb.clear(),sb.clear();
+		if(gb.empty()||sb.empty()) {gb.clear();sb.clear();}
 		return;
 	}
 	public:
+
+	/*
+	 \brief Constructor for the Candidate Elimination Class.
+	 \param r The filename
+	 \param t The target number (from 1-7)
+	 \sa Reader()
+	 */
 	CandidateElimination(Reader r,int t)
 	{
 		data=r.data;
@@ -173,7 +240,9 @@ class CandidateElimination
 		gb.push_back(q);
 		sb=nl;
 		target=t;
-		//Calculate possible values for each attribute.
+		/*!
+		 * Calculate ALL possible values for each attribute.
+		 */
 		for(int i=0;i<data[0].size()-1;i++){
 			set<int> stmp;
 			for(int j=0;j<data.size();j++) stmp.insert(data[j][i]);
@@ -193,6 +262,11 @@ class CandidateElimination
 	}
 };
 
+/*!
+   \fn main()
+   \brief Execute the Candidate Elimination Algorithm by calling the CandidateElimination Class.
+   \sa CandidateElimination(),Reader()
+ */	
 int main()
 {
 	Reader r("data.csv");
